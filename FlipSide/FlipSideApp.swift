4ae89607daf44f756015ee6658add82207c68468
DesiRorie <desirorie@icloud.com>
2023-07-10 16:46:01 -0400

    Initial Commit

---
+++ FlipSide/FlipSideApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipSideApp.swift
+//  FlipSide
+//
+//  Created by Desi Rorie on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FlipSideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
